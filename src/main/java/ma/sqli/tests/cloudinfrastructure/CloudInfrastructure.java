cb639c122c4e7ccfebbcf48e897a0ad741fe5ed2
asmai <faicalasmai8@gmail.com>
2018-11-27 17:57:46 +0000

    first commit

---
+++ src/main/java/ma/sqli/tests/cloudinfrastructure/CloudInfrastructure.java
@@ -0,0 +1,48 @@
+package ma.sqli.tests.cloudinfrastructure;
+
+public class CloudInfrastructure {
+    public void createStore(String myFiles) {
+    }
+
+    public void uploadDocument(String myFiles, String... s) {
+    }
+
+    public String listStores() {
+        return "";
+    }
+
+    public void deleteStore(String myFiles) {
+    }
+
+    public void emptyStore(String myImages) {
+    }
+
+    public void createMachine(String machine1, String linux, String s, String s1) {
+    }
+
+    public String listMachines() {
+        return "";
+    }
+
+    public void startMachine(String machine1) {
+    }
+
+    public void stopMachine(String machine1) {
+    }
+
+    public double usedMemory(String machine1) {
+        return 0.0;
+    }
+
+    public double usedDisk(String machine1) {
+        return 0.0;
+    }
+
+    public double globalUsedDisk() {
+        return 0.0;
+    }
+
+    public double globalUsedMemory() {
+        return 0.0;
+    }
+}
